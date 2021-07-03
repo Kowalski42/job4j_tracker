f70bac7a59e0024ceab0b54b704bc77758f9b684
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-07-03 13:37:15 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642]

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -12,11 +12,11 @@ public class Triangle {
     }
 
     public double semiPerimeter(double a, double b, double c) {
-        return -1;
+        return (a + b + c) / 2;
     }
 
     public boolean exist(double ab, double ac, double bc) {
-        return false;
+        return (ab + ac > bc && ab + bc > ac && ac + bc > ab) ? true : false;
     }
 
     public double area() {
@@ -26,8 +26,7 @@ public class Triangle {
         double bc = second.distance(third);
         if (this.exist(ab, ac, bc)) {
             double p = semiPerimeter(ab, ac, bc);
-            /* написать формулу для расчета площади треугольника. */
-            rsl = -1;
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
         }
         return rsl;
     }
--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -8,12 +8,22 @@ import org.junit.Test;
 public class TriangleTest {
 
     @Test
-    public void area() {
+    public void area0_0_4_0_0_4__8() {
         Point a = new Point(0, 0);
         Point b = new Point(4, 0);
         Point c = new Point(0, 4);
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
         assertThat(rsl, closeTo(8, 0.001));
+    }
+
+    @Test
+    public void area0_0_1_8_4_0__16() {
+        Point a = new Point(0, 0);
+        Point b = new Point(1, 8);
+        Point c = new Point(4, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(16, 0.001));
     }
 }
