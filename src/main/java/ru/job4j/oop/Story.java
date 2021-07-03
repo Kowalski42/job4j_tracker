b919228813ccef09cc8e43658d14439073d687a4
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-07-03 04:05:27 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Story.java
@@ -0,0 +1,43 @@
+package ru.job4j.oop;
+
+public class Story {
+    public static void main(String[] args) {
+        Ball kolobok = new Ball();
+        Hare zaychik = new Hare();
+        Wolf seryi = new Wolf();
+        Fox alisa = new Fox();
+        kolobok.rollTo(zaychik);
+        zaychik.tryEat(kolobok);
+        kolobok.rollTo(seryi);
+        seryi.tryEat(kolobok);
+        kolobok.rollTo(alisa);
+        alisa.tryEat(kolobok);
+        alisa.kill(kolobok);
+    }
+
+    public static class Ball {
+        public void rollTo(Wolf wolf) {
+        }
+        public void rollTo(Fox fox) {
+        }
+        public void rollTo(Hare hare) {
+        }
+    }
+
+    public static class Wolf {
+        public void tryEat(Ball ball) {
+        }
+    }
+
+    public static class Hare {
+        public void tryEat(Ball ball) {
+        }
+    }
+
+    public static class Fox {
+        public void tryEat(Ball ball) {
+        }
+        public void kill(Ball ball) {
+        }
+    }
+}
