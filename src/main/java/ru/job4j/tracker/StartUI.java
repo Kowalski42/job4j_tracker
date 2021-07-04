82dbf6782970d4dda88cf004372e1453a20d81bd
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-07-04 17:22:25 +0300

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+import java.time.*;
+import java.time.format.*;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item1 = new Item();
+        LocalDateTime currentDateTime = item1.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(currentDateTime.format(formatter));
+    }
+}
