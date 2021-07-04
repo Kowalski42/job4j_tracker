eedcfaffe607760b2e52a38455b69056168c2635
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-07-05 00:00:36 +0300

    4. Сравнение моделей. Метод equals [#174893]

---
+++ src/test/java/ru/job4j/pojo/LicenseTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.pojo;
+
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class LicenseTest {
+
+    @Test
+    public void eqName() {
+        License first = new License();
+        first.setOwner("Ivanov");
+        first.setModel("T6");
+        first.setCode("audio");
+        first.setCreated(new Date());
+        License second = new License();
+        second.setOwner("Ivanov");
+        second.setModel("T6");
+        second.setCode("audio");
+        second.setCreated(new Date());
+        assertThat(first, is(second));
+    }
+
+    @Test
+    public void noEqName() {
+        License first = new License();
+        first.setCode("audio");
+        License second = new License();
+        second.setCode("audio");
+        assertNotSame(first, second);
+    }
+}
