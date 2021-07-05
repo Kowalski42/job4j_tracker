efa8e9a3bc06a0c9582ada5058bc738fb37d557d
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-07-05 19:53:00 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -18,12 +18,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        for (int i = 0; i < size; i++) {
-            if (items[i].getId() == id) {
-                return items[i];
-            }
-        }
-        return null;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findByName(String key) {
@@ -35,5 +31,26 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(newItems, tempSize);
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+        }
+        item.setId(id);
+        items[index] = item;
+        return true;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
     }
 }
