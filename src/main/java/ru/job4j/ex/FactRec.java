9a01cb4875735263e894760bc23bc2fc9923d5dd
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-07-09 21:19:06 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #175905]

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -1,19 +1,12 @@
 package ru.job4j.ex;
 
 public class FactRec {
-    public static int calc(int n) throws IllegalArgumentException {
-        if (n < 0) {
-            throw new IllegalArgumentException("В качестве аргумента может быть использовано "
-                    + "только целое положительное число");
-        } else if (n <= 1) {
-            return 1;
-        } else {
-            return calc(n - 1) * n;
-        }
+    public static int calc(int n) {
+        return n < 0 ? 0 : ((n == 1 || n == 0) ? 1 : calc(n - 1) * n);
     }
 
-    public static void main(String[] args) throws IllegalArgumentException {
-        int rsl = calc(-1);
+    public static void main(String[] args) {
+        int rsl = calc(4);
         System.out.println(rsl);
     }
 }
