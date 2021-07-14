387279a411721c9cd4eb097a42fc245e45e177ed
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-07-14 22:12:20 +0300

    2. Очередь с приоритетом на LinkedList [#41670 #177604]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,29 @@
+package ru.job4j.bank;
+
+import java.util.*;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+
+    }
+
+    public void addAccount(String passport, Account account) {
+
+    }
+
+    public User findByPassport(String passport) {
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        return rsl;
+    }
+}
