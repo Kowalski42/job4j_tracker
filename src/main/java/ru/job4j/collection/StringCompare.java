836d895ed6b92af4dbc6e57878710e3775abbecf
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-07-16 01:05:46 +0300

    3. Компаратор для строк [#35008 #178100]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -1,26 +1,16 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class StringCompare implements Comparator<String> {
     @Override
-    public int compare(String left, String right) throws NullPointerException {
-        if (left == null || right == null) {
-            throw new NullPointerException();
-        }
-        char[] leftArray = left.toCharArray();
-        char[] rightArray = right.toCharArray();
-        if (leftArray.length > rightArray.length) {
-            rightArray = Arrays.copyOf(rightArray, leftArray.length);
-        } else {
-            leftArray = Arrays.copyOf(leftArray, rightArray.length);
-        }
-        for (int i = 0; i < leftArray.length; i++) {
-            if (leftArray[i] - rightArray[i] > 0 || leftArray[i] - rightArray[i] < 0) {
-                return leftArray[i] - rightArray[i];
+    public int compare(String left, String right) {
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            int temp = Integer.compare(left.charAt(i), right.charAt(i));
+            if (temp != 0) {
+                return temp;
             }
         }
-        return 0;
+        return Integer.compare(left.length(), right.length());
     }
 }
