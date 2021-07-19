55cd3d1de94787c3d1b5129d79cc25fb32dda0b3
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-07-20 00:19:22 +0300

    1. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,11 @@
+package ru.job4j.stream;
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        return students.stream().filter(predict).collect(Collectors.toList());
+    }
+}
