651e224fcf6f049b1057c6dc3644e38104fff400
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-07-19 18:17:33 +0300

    1.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -0,0 +1,16 @@
+package ru.job4j.function;
+
+import java.util.*;
+import java.util.function.*;
+
+public class SearchFolder {
+    public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
+        List<Folder> rsl = new ArrayList<>();
+        for (Folder f : list) {
+            if (pred.test(f)) {
+                rsl.add(f);
+            }
+        }
+        return rsl;
+    }
+}
