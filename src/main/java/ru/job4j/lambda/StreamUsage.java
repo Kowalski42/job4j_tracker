f410a0fce1290d3aae030c6c9d3b0c2900a7473b
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-07-19 23:39:29 +0300

    0. Stream API [#58166]

---
+++ src/main/java/ru/job4j/lambda/StreamUsage.java
@@ -0,0 +1,13 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(-100, 53, -47, 1083, 500);
+        List<Integer> list2 = list.stream().filter(i -> i > 0).collect(Collectors.toList());
+        System.out.println(list2.toString());
+    }
+}
