a4b7961f1be9e47a5deb216c3fb2281b09928c14
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-07-19 19:12:24 +0300

    3. Ссылки на методы [#70608]

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -6,6 +6,8 @@ import java.util.function.*;
 public class SearchFolder {
     public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
         List<Folder> rsl = new ArrayList<>();
+        //Consumer<Folder> consumer = (f) -> rsl.add(f);
+        //list.forEach(consumer);
         for (Folder f : list) {
             if (pred.test(f)) {
                 rsl.add(f);
