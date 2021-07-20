304372308bbfeb9815f4534430525637ba0c14e1
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-07-20 12:27:59 +0300

    1. Фильтрация учеников [#110058 #179527]

--- src/main/java/ru/job4j/stream/StreamStudent.java
+++ src/main/java/ru/job4j/stream/StreamStudent.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 
 public class StreamStudent {
     public Map<String, Student> collect(List<Student> students) {
-        return students.stream().distinct().collect(Collectors.toMap(s -> s.getSurname(), s -> s));
+        return students.stream()
+                .distinct()
+                .collect(Collectors.toMap(s -> s.getSurname(), s -> s));
     }
 }
