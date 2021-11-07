62d8e481255efeb49a280e33f23e6ac8c7b31ea5
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-11-07 11:58:03 +0300

    2. Отсортировать департаменты [#34352] Correct the code in class Departments and class DepDescComp

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,15 +8,6 @@ public class DepDescComp implements Comparator<String> {
         String[] temp1 = o1.split("/");
         String[] temp2 = o2.split("/");
         int temp = temp2[0].compareTo(temp1[0]);
-        if (temp != 0) {
-            return temp;
-        }
-        for (int i = 1; i < Math.min(temp1.length, temp2.length); i++) {
-            temp = temp1[i].compareTo(temp2[i]);
-            if (temp != 0) {
-                return temp;
-            }
-        }
-        return Integer.compare(o1.length(), o2.length());
+        return temp != 0 ? temp : o1.compareTo(o2);
     }
 }
--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -10,11 +10,7 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                if (!start.equals("")) {
-                    start += "/" + el;
-                } else {
-                    start = el;
-                }
+                start += (start.equals("")) ? el : ("/" + el);
                 tmp.add(start);
             }
         }
