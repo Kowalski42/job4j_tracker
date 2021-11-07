a3248c0b625bb5b1334b6f30ef281b8af951bed3
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-11-08 00:19:53 +0300

    2. Отсортировать департаменты [#34352] Correct the code in class Departments and class DepDescComp,v2

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -10,7 +10,7 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                start += ("".equals(start)) ? el : ("/" + el);
+                start += "".equals(start) ? el : "/" + el;
                 tmp.add(start);
             }
         }
